76c95c90f82f105fdc30a173088d444286e93a41
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-07 11:50:12 +0530

    feat: add server setup with dotenv

---
+++ server/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const app = require('./src/app')
+
+const PORT = process.env.PORT || 2000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server runs on http://localhost:${PORT}`)
+});
