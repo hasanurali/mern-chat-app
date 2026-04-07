127b7d2b14be796f4124c985e663d3b24bce4825
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-07 12:04:57 +0530

    feat: add constant file

---
+++ server/src/constant/index.js
@@ -0,0 +1,13 @@
+// <---STATUS CODES--->
+module.exports.HTTP_STATUS = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    DUPLICATE_KEY: 409
+};
+
+// <---BCRYPT_SALT_ROUNDS--->
+module.exports.BCRYPT_SALT_ROUNDS = 10;
+
