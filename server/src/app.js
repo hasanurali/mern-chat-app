7be57f770506810e8ce4780894de34af3f2713b8
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-07 17:43:22 +0530

    feat: add auth route

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const authRoute = require('./routes/auth.route')
+
 const errorHandler = require('./middlewares/error.middleware');
 
 const app = express()
+
+
+// Routes
+app.use('/api/v1/auth', authRoute)
 
 
 app.use(errorHandler)
---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const authRoute = express.Router()
+const { registerUser, loginUser, getCurrentUser, logoutUser, refreshToken } = require('../controllers/auth.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
+const { registerValidation, loginValidation } = require('../validations/auth.validation')
+const validate = require('../middlewares/validation.result.middleware')
+
+
+authRoute.post('/register',
+    registerValidation,
+    validate,
+    registerUser
+);
+
+authRoute.post('/login',
+    loginValidation,
+    validate,
+    loginUser
+);
+
+authRoute.get('/me',
+    authMiddleware,
+    getCurrentUser
+);
+
+authRoute.post('/logout',
+    authMiddleware,
+    logoutUser
+);
+
+authRoute.post('/refresh',
+    refreshToken
+);
+
+
+module.exports = authRoute;
