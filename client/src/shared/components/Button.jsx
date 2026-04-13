b108a854e5fa77634c37c49096b2d2604976d790
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-13 21:21:05 +0530

    style: add disabled styles to Button component

--- client/src/shared/components/Button.jsx
+++ client/src/shared/components/Button.jsx
@@ -5,11 +5,13 @@ const Button = ({
     disabled = false,
     ...props
 }) => {
+
     return (
         <button
             type={type}
             disabled={disabled}
-            className={`w-full font-semibold px-10 py-3 rounded-full text-white shadow-md hover:opacity-90 transition-opacity cursor-pointer ${className}`}
+            className={`w-full font-semibold px-10 py-3 rounded-full text-white shadow-md hover:opacity-90 transition-opacity cursor-pointer ${className}
+             disabled:bg-blue-300 disabled:opacity-60 disabled:cursor-not-allowed disabled:shadow-none`}
             {...props}
         >
             {children}
