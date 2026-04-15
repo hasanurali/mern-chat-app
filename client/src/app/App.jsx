ed1277fb872b1f189b67fe46cb73d10dd6780e91
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-15 19:43:16 +0530

    feat: add toast notifications using react-toastify

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import AppRoutes from './AppRoutes'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
   return (
     <>
-    <AppRoutes/>
+      <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        pauseOnFocusLoss
+        draggable
+        draggablePercent={60}
+        limit={3}
+        theme="colored"
+      />
     </>
   )
 }
