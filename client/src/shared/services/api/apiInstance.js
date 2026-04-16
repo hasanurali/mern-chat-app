04f587de206dae9ce4c594ae580b2fbcd3e0a5ac
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-16 20:22:27 +0530

    feat: handle 401 responses with token refresh interceptor

--- client/src/shared/services/api/apiInstance.js
+++ client/src/shared/services/api/apiInstance.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { refreshToken } from "@/features/index"
+import { ENDPOINTS } from './apiEndpoint'
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -6,12 +8,59 @@ export const api = axios.create({
 });
 
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error) => {
+  failedQueue.forEach((promise) => {
+    if (error) {
+      promise.reject(error)
+    } else {
+      promise.resolve();
+    }
+  });
+
+  failedQueue = [];
+};
+
 api.interceptors.response.use(
   (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      console.log("Unauthorized");
-    }
+  async (err) => {
+
+    const originalRequest = err.config;
+    const isLoginAndRefreshRoute = originalRequest?.url.includes(ENDPOINTS.AUTH.LOGIN) || originalRequest?.url.includes(ENDPOINTS.AUTH.REFRESH);
+
+    if (err.response?.status === 401 && !originalRequest._retry && !isLoginAndRefreshRoute) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({
+            resolve: () => resolve(api(originalRequest)),
+            reject
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+
+        await refreshToken()
+        processQueue(null)
+        return api(originalRequest);
+
+      } catch (refreshErr) {
+
+        processQueue(refreshErr);
+        window.location.href = "/login";
+        return Promise.reject(refreshErr);
+
+      } finally {
+        isRefreshing = false;
+      };
+
+    };
     return Promise.reject(err);
   }
 );
