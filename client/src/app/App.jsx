b67944d37421662daafe25efb3d4dd9d641ba7d6
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-17 09:51:44 +0530

    feat: initialize user session on app load

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppRoutes from './AppRoutes'
 import { ToastContainer } from 'react-toastify'
+import { useSelector, useDispatch } from "react-redux"
+import { getCurrentUser, initializeUser, clearUser } from "@/features/index"
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const [checkAuth, setCheckAuth] = useState(false);
+
+  const user = useSelector((state) => state.auth.userData);
+  const dispatch = useDispatch()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === "/auth/login") {
+      setCheckAuth(true)
+      return;
+    };
+
+    getCurrentUser()
+      .then(res => dispatch(initializeUser(res.data.data)))
+      .catch(() => dispatch(clearUser()))
+      .finally(() => setCheckAuth(true));
+  }, [])
+
+  if (!checkAuth) return <div>Loding...</div>
+
   return (
     <>
       <AppRoutes />
