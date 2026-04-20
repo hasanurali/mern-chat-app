89d748ae38010ae5e86c0d87ca8fd6b04fe1cc77
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-20 11:06:29 +0530

    feat: add group chat validation

---
+++ server/src/validations/chat.validation.js
@@ -0,0 +1,17 @@
+const { body } = require("express-validator")
+
+module.exports.groupChatValidation = [
+
+    // name validation
+    body("name")
+        .notEmpty().withMessage("Name is required")
+        .isLength({ min: 3 }).withMessage("Name must be at least 3 characters"),
+
+    // participants validation
+    body("participants")
+        .isArray({ min: 2 }).withMessage("Participants must be an array with at least 2 users"),
+
+    // each participant should be string (userId)
+    body("participants.*")
+        .isString().withMessage("Each participant must be a string")
+]
