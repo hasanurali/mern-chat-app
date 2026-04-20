4e5b3bced8666805220acd3aa2224e73d77d9b34
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-20 11:07:37 +0530

    feat: add group chat route

--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const chatRoute = express.Router()
 const authMiddleware = require('../middlewares/auth.middleware')
-const { createOneToOneChat } = require('../controllers/chat.controller')
+const { createOneToOneChat, createGroupChat } = require('../controllers/chat.controller');
+const { groupChatValidation } = require('../validations/chat.validation');
+const validate = require('../middlewares/validation.result.middleware');
 
 
 chatRoute.post('/one/:id',
     authMiddleware,
     createOneToOneChat
+);
+
+chatRoute.post('/group',
+    authMiddleware,
+    groupChatValidation,
+    validate,
+    createGroupChat
 );
 
 module.exports = chatRoute;
