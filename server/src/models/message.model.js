b74b9d8c950feefb15cbd02f34597a310228cb93
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-04-22 08:48:29 +0530

    feat: add message model

---
+++ server/src/models/message.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    chat: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chat",
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: "unseen",
+        enum: ["seen", "unseen"]
+    }
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model("Message", messageSchema);
